9dce6358259203dde9be0d0a5d78dc262c38f881
Mariam Arshad <d3676772@live.tees.ac.uk>
2024-04-22 18:05:35 +0100

    Lets develop Android Application!

---
+++ app/src/main/java/com/example/explore_with_samay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.explore_with_samay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
